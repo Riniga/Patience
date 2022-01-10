d63942a1ccc8597887b63fa170b891737d25056f
Rickard Nisses-Gagnér <rickard@nisses-gagner.se>
2022-01-10 08:15:03 +0100

    Add missed files

---
+++ Deck.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patience
+{
+    public class Deck : Stack<PlayingCard>
+    {
+        public bool AceHigh = true;
+        public Deck()
+        {
+            foreach (Suit suit in (Suit[])Enum.GetValues(typeof(Suit)))
+            {
+                for (int i = 0; i < 13; i++)
+                {
+                    Push(new PlayingCard(suit, (Rank)i ));
+                }
+            }
+        }
+        public void Scramble()
+        {
+            Random random = new Random();
+            var cards = this.ToList();
+            Stack<PlayingCard> result= new Stack<PlayingCard> ();
+            while (cards.Count > 0)
+            { 
+                int id = random.Next(cards.Count);
+                result.Push(cards.ElementAt(id));
+                cards.RemoveAt(id);
+            }
+            Clear();
+            foreach (var item in result) Push(item);
+        }
+        public void Print()
+        {
+            foreach (var card in this)
+            {
+                Console.WriteLine(card.ToString());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Patience;
+
+PlayIdioten(100000);
+
+void PlayIdioten(int numberofgames)
+{ 
+    Console.WriteLine($"Play 'idioten' {numberofgames} times!");
+    Deck copy = new();
+    Deck winner = new();
+
+    int winning = 0;
+    for (int i = 0;i< numberofgames; i++)
+    { 
+        Deck deck = new Deck();
+        deck.Scramble();
+        copy.Clear();
+        foreach (var card in deck) copy.Push(card);
+
+        Patience.Idioten game = new Patience.Idioten(deck);
+
+        if (game.Play())
+        {
+            winning++;
+            winner.Clear();
+            foreach (var card in copy) winner.Push(card); 
+        }
+    }
+
+    Console.WriteLine("Winning Deck");
+    Console.WriteLine("---------------");
+    winner.Print();
+    Console.WriteLine($"Number of winning games = {winning} this is {((double)winning*100 / numberofgames)} %");
+}
